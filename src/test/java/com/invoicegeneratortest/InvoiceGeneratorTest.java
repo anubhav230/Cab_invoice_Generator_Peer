b6022c49ed75ed971a906fc4d6a51b37c7d76dd3
PrajyotGajane <prajyotgajane@gmail.com>
2020-07-10 23:28:11 +0530

    ADDED- Test case for premium with mininum fare

--- src/test/java/com/invoicegeneratortest/InvoiceGeneratorTest.java
+++ src/test/java/com/invoicegeneratortest/InvoiceGeneratorTest.java
@@ -52,4 +52,20 @@ public class InvoiceGeneratorTest {
             InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(4, 145.0);
             Assert.assertEquals(expectedInvoiceSummary, summary);
       }
+      @Test
+      public void givenPremiumUserId_GenerateTotalFare_ShouldReturnInvoiceSummery() {
+            Ride[] rides = {new Ride(RideType.PREMIUM,35.0, 45), new Ride(RideType.PREMIUM,10.55, 30), new Ride(RideType.NORMAL,20, 30)};
+            invoiceGenerator.addRides("Anubhav", rides);
+            InvoiceSummary invoiceSummery = invoiceGenerator.getInvoiceSummary("Anubhav");
+            InvoiceSummary expectedSummery = new InvoiceSummary(3, 1063.25);
+            Assert.assertEquals(expectedSummery, invoiceSummery);
+      }
+      @Test
+      public void givenPremiumLessDistanceOrTime_ShouldReturnMinFare() {
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+            double distance = 0.1;
+            int time = 1;
+            double fare = invoiceGenerator.calculateFare(RideType.PREMIUM, distance, time);
+            Assert.assertEquals(20, fare, 0.0);
+      }
 }
