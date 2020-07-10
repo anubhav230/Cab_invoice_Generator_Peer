af6e454fdd2deaebc1a0ee735f1dd5538d09e41b
anubhav230 <anubhavsingh230@gmail.com>
2020-07-10 17:58:22 +0530

    ADDED- Test case for less distance or time

--- src/test/java/com/invoicegeneratortest/InvoiceGeneratorTest.java
+++ src/test/java/com/invoicegeneratortest/InvoiceGeneratorTest.java
@@ -13,4 +13,14 @@ public class InvoiceGeneratorTest {
         double fare = invoiceGenerator.calculateFare(distance, time);
         Assert.assertEquals(25, fare,0.0);
     }
+
+    @Test
+    public void givenLessDistanceOrTime_ShouldReturnMinFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance = 0.1;
+        int time = 1;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(5, fare,0.0);
+
+    }
 }
