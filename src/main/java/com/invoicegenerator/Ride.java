c6206c20c81eec28365105e7d761fbceccd94631
anubhav230 <anubhavsingh230@gmail.com>
2020-07-10 23:22:34 +0530

    ADDED- enum constant for normal and premium

--- src/main/java/com/invoicegenerator/Ride.java
+++ src/main/java/com/invoicegenerator/Ride.java
@@ -1,11 +1,15 @@
 package com.invoicegenerator;
 
+import com.invoicegeneratortest.RideType;
+
 public class Ride {
     public final double distance;
     public final int time;
+    public final RideType rideType;
 
-    public Ride(double distance, int time) {
+    public Ride(RideType rideType, double distance, int time) {
         this.distance = distance;
         this.time = time;
+        this.rideType = rideType;
     }
 }
