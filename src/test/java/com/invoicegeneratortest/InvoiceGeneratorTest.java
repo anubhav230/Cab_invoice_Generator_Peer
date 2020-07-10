51be75ef737994601225114e80055fb9ccebca28
anubhav230 <anubhavsingh230@gmail.com>
2020-07-11 03:10:13 +0530

    ADDED- test case for premium customer

--- src/test/java/com/invoicegeneratortest/InvoiceGeneratorTest.java
+++ src/test/java/com/invoicegeneratortest/InvoiceGeneratorTest.java
@@ -35,8 +35,10 @@ public class InvoiceGeneratorTest {
 
       @Test
       public void givenMultipleRide_ShouldReturnInvoiceSummary() {
-            Ride[] rides = {new Ride(RideType.NORMAL,2.0, 5), new Ride(RideType.NORMAL,0.1, 1),
-                    new Ride(RideType.NORMAL,4.0, 25), new Ride(RideType.NORMAL,3.0, 20)};
+            Ride[] rides = {new Ride(RideType.NORMAL,2.0, 5),
+                            new Ride(RideType.NORMAL,0.1, 1),
+                            new Ride(RideType.NORMAL,4.0, 25),
+                            new Ride(RideType.NORMAL,3.0, 20)};
             InvoiceSummary summary = invoiceGenerator.calculateFare(rides);
             InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(4, 145.0);
             Assert.assertEquals(expectedInvoiceSummary, summary);
@@ -45,16 +47,21 @@ public class InvoiceGeneratorTest {
       @Test
       public void givenUserIdAndRides_ShouldReturnInvoiceSummary() {
             String userId = "Prajyot";
-            Ride[] rides = {new Ride(RideType.NORMAL,2.0, 5), new Ride(RideType.NORMAL,0.1, 1),
-                    new Ride(RideType.NORMAL,4.0, 25), new Ride(RideType.NORMAL,3.0, 20)};
+            Ride[] rides = {new Ride(RideType.NORMAL,2.0, 5),
+                            new Ride(RideType.NORMAL,0.1, 1),
+                            new Ride(RideType.NORMAL,4.0, 25),
+                            new Ride(RideType.NORMAL,3.0, 20)};
             invoiceGenerator.addRides(userId, rides);
             InvoiceSummary summary = invoiceGenerator.getInvoiceSummary(userId);
             InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(4, 145.0);
             Assert.assertEquals(expectedInvoiceSummary, summary);
       }
+
       @Test
       public void givenPremiumUserId_GenerateTotalFare_ShouldReturnInvoiceSummery() {
-            Ride[] rides = {new Ride(RideType.PREMIUM,35.0, 45), new Ride(RideType.PREMIUM,10.55, 30), new Ride(RideType.NORMAL,20, 30)};
+            Ride[] rides = {new Ride(RideType.PREMIUM,35.0, 45),
+                            new Ride(RideType.PREMIUM,10.55, 30),
+                            new Ride(RideType.NORMAL,20, 30)};
             invoiceGenerator.addRides("Anubhav", rides);
             InvoiceSummary invoiceSummery = invoiceGenerator.getInvoiceSummary("Anubhav");
             InvoiceSummary expectedSummery = new InvoiceSummary(3, 1063.25);
